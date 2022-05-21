8de31b4fa8eba59ab43d2e55802ef81c91684d4b
nrnkt <ripsivis@gmail.com>
2022-05-21 18:10:09 +0300

    ğ2

--- dist/assets/useTitle.47c5e0a5.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./vendor.24a792c2.js";const r=(e,t=!1)=>{o.exports.useEffect(()=>{const s=document.title;return t?document.title=`${e} - alist`:document.title=e,()=>{document.title=s}},[])};var u=r;export{u};
